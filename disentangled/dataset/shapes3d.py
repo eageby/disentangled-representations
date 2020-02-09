b2f84db149a03ba1caf6d6cdf4d58b11d083c815
Elias Ågeby <eageby@kth.se>
2020-02-09 18:19:43 +0100

    Generalized to a dataset core
    Made core functions general by accessing module variables of calling
    dataset

--- disentangled/dataset/shapes3d.py
+++ disentangled/dataset/shapes3d.py
@@ -1,16 +1,11 @@
+import sys 
 import tensorflow_datasets as tfds
-import matplotlib.pyplot
+
+from . import core
+core._module = sys.modules[__name__]
+from .core import *
 
 _name = "shapes3d"
 _version = "2.0.0"
 
 _builder = tfds.builder("{}:{}".format(_name, _version))
-
-info = _builder.info
-
-def load(split='train'):
-    return _builder.as_dataset(split=split)
-
-def show(**kwargs):
-    return tfds.show_examples(info, _builder.as_dataset(split='train'), **kwargs)
-
