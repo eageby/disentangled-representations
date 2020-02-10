7e83c21c050a377339deb83e67371642af562e7a
Elias Ågeby <eageby@kth.se>
2020-02-10 10:04:01 +0100

    changed version attribute to conventional name

--- disentangled/dataset/shapes3d.py
+++ disentangled/dataset/shapes3d.py
@@ -6,6 +6,6 @@ core._module = sys.modules[__name__]
 from .core import *
 
 _name = "shapes3d"
-_version = "2.0.0"
+__version__ = "2.0.0"
 
-_builder = tfds.builder("{}:{}".format(_name, _version))
+_builder = tfds.builder("{}:{}".format(_name, __version__))
