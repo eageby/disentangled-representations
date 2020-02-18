6563e1c0dd3f96cdc10157c92f9c217a117df6c3
Elias Ågeby <eageby@kth.se>
2020-02-18 17:41:44 +0100

    updated result visual

--- disentangled/visualize.py
+++ disentangled/visualize.py
@@ -3,16 +3,34 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+import disentangled.dataset.utils as utils
+
+"""Disable toolbar in matplotlib"""
 matplotlib.rcParams['toolbar'] = 'None'
 
-def stack(dataset, rows, cols):
-    if isinstance(dataset, tf.data.Dataset):
-        dataset = dataset.take(rows*cols)
-        map_ = lambda x: x['image']
-        dataset = np.asarray(list(dataset.map(map_).as_numpy_iterator()))
-    image_rows = [np.concatenate(dataset[cols*i:cols*(i+1)], axis=1) for i in range(rows)]
+def normalize(images):
+    max_ = images.max(axis=(1,2,3))
+    return images/np.expand_dims(max_, axis=(1,2,3))
+    
+def _row(data):
+    data = np.split(data, data.shape[0], axis=0)
+    return np.squeeze(np.concatenate(data, axis=2), axis =0)
 
-    return np.concatenate(image_rows, axis=0)
+def results(target, encoded, rows=4, cols=8):
+    if isinstance(target, tf.data.Dataset):
+        target = utils.numpy(target)
+
+    if isinstance(encoded, tf.data.Dataset):
+        encoded = utils.numpy(encoded)
+         
+    # target = normalize(target)
+    # encoded = normalize(encoded)
+
+    target_rows = [_row(target[cols*i:cols*(i+1)]) for i in range(int(rows/2))]
+    encoded_rows = [_row(encoded[cols*i:cols*(i+1)]) for i in range(int(rows/2))]
+    
+    image_rows = [element for pair in zip(target_rows, encoded_rows) for element in pair]
+    show(np.concatenate(image_rows, axis=0))
 
 def show(image):
     image = np.squeeze(image)
