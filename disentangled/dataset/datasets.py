7f9d1008c7cc2b51b54dd707fb91fa9e2bf67c2c
Elias Ågeby <eageby@kth.se>
2020-02-18 10:49:13 +0100

    Docstrings

--- disentangled/dataset/datasets.py
+++ disentangled/dataset/datasets.py
@@ -1,34 +1,57 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+
 class Dataset:
+    """Abstract Base Class for datasets, interfacing the tensorflow_datasets API"""
+
     @classmethod
-    def load(cls, split="train"):
+    def load(cls, split="train") -> tf.data.Dataset:
+        """Loads an instance of the tensorflow dataset.
+
+        Args:
+            split:  From tensorflow_datasets doc https://www.tensorflow.org/datasets/api_docs/python/tfds/core/DatasetBuilder#as_dataset.
+                    Which subset(s) of the data to read.
+                    If None (default), returns all splits in a dict <key: tfds.Split, value: tf.data.Dataset>.
+        Returns:
+            tf.data.Dataset
+        """
+
         return cls._builder.as_dataset(split=split)
 
     @classmethod
-    def show(cls,**kwargs):
-        cls._builder.download_and_prepare()
+    def show(cls, **kwargs):
+        """Shows examples of the dataset"""
+
         return tfds.show_examples(
             cls.info, cls._builder.as_dataset(split="train"), **kwargs
         )
 
     @classmethod
     def version(cls):
+        """Returns the version used."""
+
         return cls.__version__
 
     @classmethod
     def info(cls):
+        """Returns information about the dataset. """
+
         return cls._builder.info
 
 
 class MNIST(Dataset):
+    """MNIST Dataset"""
+
     _name = "mnist"
     __version__ = "3.0.0"
 
     _builder = tfds.builder("{}:{}".format(_name, __version__))
 
+
 class Shapes3d(Dataset):
+    """Shapes3d Dataset"""
+
     _name = "shapes3d"
     __version__ = "2.0.0"
 
--- disentangled/dataset/utils.py
+++ disentangled/dataset/utils.py
@@ -1,17 +1,41 @@
-__all__ = ['binary', 'get_image', 'numpy']
+__all__ = ["binary", "get_image", "numpy"]
 
 import numpy as np
 import tensorflow as tf
 
 
 def binary(element):
+    """Binarizes the element in the dataset.
+    
+    Requires that the dataset is populated with only one element.
+    Recommended usage is to call get_image first, as dataset.map(get_image).map(binary).
+
+    Args:
+        element: Element to be mapped
+
+    Returns:
+        Binary quantized element
+    """
     return tf.quantization.fake_quant_with_min_max_args(
         tf.cast(element, tf.float32), min=0, max=1
     )
 
+
 def get_image(element):
+    """Gets the image feature of the element in a dataset."""
     return tf.cast(element["image"], tf.float32)
 
 
 def numpy(dataset):
+    """Converts a dataset to a numpy array.
+
+    Requires that the dataset is populated with only one element.
+    Batch dimension at axis 0.
+
+    Args:
+        dataset (tf.data.Dataset): An instance of a dataset.
+
+    Returns:
+        (np.array)
+    """
     return np.asarray(list(data.as_numpy_iterator()))
