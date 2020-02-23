541297d72cf53967c141dbfb8ad67712b15e9368
Elias Ågeby <eageby@kth.se>
2020-02-23 16:48:08 +0100

    simpler main

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import disentangled.dataset as dataset
-
-from disentangled.model.vae import *
-import disentangled.model.objective as objective
 import tensorflow as tf
 
+import disentangled.dataset as dataset
+import disentangled.model.models as models
+import disentangled.model.utils as modelutils
 import disentangled.visualize as vi
 
-def mnist():
-    data = dataset.mnist.load().map(dataset.utils.get_image).batch(128)
-    model= VAE(Encoder(20), Representation(), Decoder('relu'), objective.BetaVAE(gaussian=True))
-
-    model.compile(tf.keras.optimizers.Adam(learning_rate=1e-4))
-    model.fit(data, epochs=5)
-
-    estimate, representation, target = model.predict(data, steps=1)
-
-    vi.results(target, estimate, 5, 10)
 
 def shapes3d():
-    data = dataset.shapes3d.load().take(128000).map(dataset.utils.get_image).map(dataset.utils.normalize_uint8).batch(128)
-    model= VAE(Encoder(20), Representation(), Decoder('sigmoid'), objective.BetaVAE(gaussian=True))
+    data = dataset.shapes3d.pipeline(batch_size=256).take(500)
+    model = models.AEVB(latents=32)  
 
     model.compile(tf.keras.optimizers.Adam(learning_rate=1e-4))
-    model.fit(data, epochs=5)
+    model.fit(data, epochs=1)
 
     estimate, representation, target = model.predict(data, steps=1)
-   
+    vi.results(target, estimate, 5, 10)
+    modelutils.save(model, 'shapes3d')
+
+
+def continue_training():
+    model = modelutils.load('shapes3d')
+    data = dataset.shapes3d.pipeline(batch_size=256).take(500)
+    model.fit(data, epochs=1)
+    modelutils.save(model, 'shapes3d')
+
+    estimate, representation, target = model.predict(data, steps=1)
     vi.results(target, estimate, 5, 10)
 
 shapes3d()
+
