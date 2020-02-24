9fa9730186c68e9a31aca6e6f54972cc5b054031
Elias Ågeby <eageby@kth.se>
2020-02-24 17:01:46 +0100

    mobilenet

--- disentangled/model/conv.py
+++ disentangled/model/conv.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
 import disentangled.model.activations as activations
 
+class MobilenetV2(tf.keras.layers.Layer):
+
+    def build(self, input_dim):
+        self.trainable = False
+        self.model = tf.keras.applications.MobileNetV2(input_shape=input_dim, include_top=False, pooling=None, weights='imagenet')
+    
+    def call(self, x):
+        return self.model(x)
 
 class Encoder(tf.keras.Sequential):
     def __init__(self, **kwargs):
@@ -11,12 +19,12 @@ class Encoder(tf.keras.Sequential):
             tf.keras.layers.Conv2D(
                 32, kernel_size=(4, 4), strides=(2, 2), activation="relu"
             ),
-            tf.keras.layers.Conv2D(
-                64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
-            ),
-            tf.keras.layers.Conv2D(
-                64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
-            ),
+            # tf.keras.layers.Conv2D(
+            #     64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
+            # ),
+            # tf.keras.layers.Conv2D(
+            #     64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
+            # ),
         ]
 
         super(Encoder, self).__init__(layers, **kwargs)
@@ -33,19 +41,19 @@ class Decoder(tf.keras.layers.Layer):
             tf.keras.layers.Conv2DTranspose(
                 32, kernel_size=(4, 4), strides=(2, 2), activation="relu"
             ),
-            tf.keras.layers.Conv2DTranspose(
-                64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
-            ),
-            tf.keras.layers.Conv2DTranspose(
-                64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
-            ),
+            # tf.keras.layers.Conv2DTranspose(
+            #     64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
+            # ),
+            # tf.keras.layers.Conv2DTranspose(
+            #     64, kernel_size=(4, 4), strides=(2, 2), activation="relu"
+            # ),
         ]
         self.mean_layer = tf.keras.layers.Conv2DTranspose(
-                3, kernel_size=(3, 3), strides=(1, 1), activation=activations.relu1
+                1, kernel_size=(3, 3), strides=(1, 1), activation=activations.relu1
             )
 
         self.log_var_layer = tf.keras.layers.Conv2DTranspose(
-                3, kernel_size=(3, 3), strides=(1, 1), activation=activations.relu1
+                1, kernel_size=(3, 3), strides=(1, 1), activation="relu"
             )
         
     
