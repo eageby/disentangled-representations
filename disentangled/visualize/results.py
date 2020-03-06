7f01a8e233519918482aca636c585a08e2f54d42
Elias Ågeby <eageby@kth.se>
2020-03-06 10:54:59 +0100

    Improved cli for latentspace

--- disentangled/visualize/results.py
+++ disentangled/visualize/results.py
@@ -1,6 +1,8 @@
 from . import show 
 
 def results(model, dataset, rows, cols):
+    model.compile('adam', loss=lambda _, __: 0.0)
+
     reconstructed, representation, target = model.predict(dataset.pipeline(),steps=10)
     
     show.results(target, reconstructed, rows, cols)
--- main.py
+++ main.py
@@ -1,76 +1,115 @@
-import click
 from pathlib import Path
 
-import disentangled.training
-import disentangled.model
-import disentangled.visualize.latentspace
-import disentangled.utils
+import click
+
 import disentangled.hyperparameters
+import disentangled.model
+import disentangled.training
+import disentangled.utils
+import disentangled.visualize.latentspace
+
+_MODELS = ["betavae_mnist", "betavae_shapes3d"]
+_DATASETS = {("betavae_mnist"): "MNIST", ("betavae_shapes3d"): "Shapes3d"}
 
 
-@click.group(chain=True, context_settings={'help_option_names':['-h','--help']})
-@click.argument('model', type=click.Choice(['betavae_mnist', 'betavae_shapes3d'], case_sensitive=True))
-@click.option('--no-gpu', is_flag=True) 
+@click.group(chain=True, context_settings={"help_option_names": ["-h", "--help"]})
+@click.argument(
+    "model", type=click.Choice(_MODELS, case_sensitive=True),
+)
+@click.option("--no-gpu", is_flag=True, default=False)
+@click.option(
+    "--directory",
+    "-d",
+    type=click.Path(writable=True),
+    default=Path("./models"),
+    show_default=True,
+)
 @click.pass_context
-def cli(ctx, model, no_gpu):
+def cli(ctx, model, no_gpu, directory):
     if no_gpu:
         disentangled.utils.disable_gpu()
-         
-    mnist_models = ['betavae_mnist']
-    shapes3d_models = ['betavae_shapes3d']
-    
-    if model in mnist_models:
-        dataset = 'MNIST'
-    else: 
-        dataset = 'Shapes3d'
-    
-    ctx.ensure_object(dict)
 
-    ctx.obj['model_name'] = model
-    ctx.obj['model'] = disentangled.model.get(model)
-    ctx.obj['dataset_name'] = dataset
-    ctx.obj['dataset'] = disentangled.dataset.get(dataset) 
+    dataset = next(val for key, val in _DATASETS.items() if model in key)
+
+    ctx.ensure_object(dict)
+    ctx.obj['directory'] = directory
+
+    ctx.obj["model_name"] = model
+    ctx.obj["model"] = disentangled.model.utils.load(model, directory)
+    ctx.obj["dataset_name"] = dataset
+    ctx.obj["dataset"] = disentangled.dataset.get(dataset)
+
 
 @cli.command()
-@click.option('--optimizer','-o', type=str)
-@click.option('--learning_rate','-l', type=float)
-@click.option('--batch_size','-b', type=int)
-@click.option('--iterations','-i', type=float)
-@click.option('--save/--no-save','-s', default=True)
-@click.option('--overwrite', is_flag=True, default=False)
-@click.option('--directory', '-d', type=click.Path(writable=True), default=Path('./models'), show_default=True)
-@click.option('--show_default', '-D', is_flag=True)
+@click.option("--optimizer", "-o", type=str)
+@click.option("--learning_rate", "-l", type=float)
+@click.option("--batch_size", "-b", type=int)
+@click.option("--iterations", "-i", type=float)
+@click.option("--save/--no-save", "-s", default=True)
+@click.option("--overwrite", is_flag=True, default=False)
+@click.option("--show_default", "-D", is_flag=True)
 @click.pass_context
-def train(ctx, save, overwrite, directory, show_default, **kwargs):
+def train(ctx, save, overwrite, show_default, **kwargs):
     """train model"""
+
     if show_default:
-        disentangled.hyperparameters.print_default(ctx.obj['model_name'])
+        disentangled.hyperparameters.print_default(ctx.obj["model_name"])
+
         return
 
-    hyperparameters = disentangled.hyperparameters.complete(ctx.obj['model_name'], kwargs)
-    disentangled.hyperparameters.print_(ctx.obj['model_name'], hyperparameters, ctx.obj['dataset_name'])
+    hyperparameters = disentangled.hyperparameters.complete(
+        ctx.obj["model_name"], kwargs
+    )
+    disentangled.hyperparameters.print_(
+        ctx.obj["model_name"], hyperparameters, ctx.obj["dataset_name"]
+    )
 
     model = disentangled.training.train(
-        ctx.obj['model'], ctx.obj['dataset'], hyperparameters=hyperparameters
-    )  
+        ctx.obj["model"], ctx.obj["dataset"], hyperparameters=hyperparameters
+    )
 
     if save:
-        disentangled.model.save(model, ctx.obj['model_name'], dir_=directory, overwrite=overwrite)
+        disentangled.model.save(
+            model, ctx.obj["model_name"], dir_=directory, overwrite=overwrite
+        )
 
-    ctx.obj['model'] = model
+    ctx.obj["model"] = model
+
 
 @cli.command()
-@click.option('--rows', '-r', type=int, default=4)
-@click.option('--cols', '-c', type=int, default=8)
+@click.option("--rows", "-r", type=int, default=4)
+@click.option("--cols", "-c", type=int, default=8)
 @click.pass_context
-def results(ctx, rows, cols):
+def results(ctx, **kwargs):
     """Compare reconstruction with target"""
-    disentangled.visualize.results(ctx.obj['model'], ctx.obj['dataset'], rows, cols)
+    disentangled.visualize.results(ctx.obj["model"], ctx.obj["dataset"], **kwargs)
 
-@cli.command('latentspace')
+
+@cli.command()
+@click.option("--shuffle", is_flag=True, default=False)
+@click.option("--steps", type=int, default=41)
+@click.option("--sample", "-s", type=int, default=0)
+@click.option("--dimensions", type=int, default=10)
+@click.option("--offset", "-o", type=int, default=0)
 @click.pass_context
-def latentspace(ctx):
-    """Latent space traversal visual"""
-    disentangled.visualize.latentspace.traversal_1d(ctx.obj['model'], ctx.obj['dataset'])
+def latent1d(ctx, **kwargs):
+    """Latent space traversal in 1D"""
+    disentangled.visualize.latentspace.traversal_1d(
+        ctx.obj["model"], ctx.obj["dataset"], **kwargs
+    )
+
+
+@cli.command()
+@click.option("--shuffle", is_flag=True, default=False)
+@click.option("--steps", type=int, default=20)
+@click.option("--sample", "-s", type=int, default=0)
+@click.option("--offset", "-o", type=int, default=0)
+@click.pass_context
+def latent2d(ctx, **kwargs):
+    """Latent space traversal in 2D"""
+    disentangled.visualize.latentspace.traversal_2d(
+        ctx.obj["model"], ctx.obj["dataset"], **kwargs
+    )
+
 
 cli()
