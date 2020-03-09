88ea4c886944369a4495991c95e43dbaa67d4b0c
Elias Ågeby <eageby@kth.se>
2020-03-09 13:54:18 +0100

    simplified optimizer

--- disentangled/hyperparameters.py
+++ disentangled/hyperparameters.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def complete(model_name: str, hyperparameters: dict) -> dict:
     hyperparameters = {
         key: hyperparameters[key]
@@ -10,9 +11,9 @@ def complete(model_name: str, hyperparameters: dict) -> dict:
     }
     hyperparameters_default = get_default(model_name)
     hyperparameters_default.update(hyperparameters)
-    hyperparameters_default['optimizer'] = optimizer_identifier(hyperparameters)
 
-    return hyperparameters_default 
+    return hyperparameters_default
+
 
 def get_default(model_name: str) -> dict:
     return getattr(sys.modules[__name__], model_name)
@@ -20,7 +21,9 @@ def get_default(model_name: str) -> dict:
 
 def print_default(model_name: str) -> None:
     message = """{}
-Default Hyperparameters""".format("\u2500" * 40)
+Default Hyperparameters""".format(
+        "\u2500" * 40
+    )
     print(message)
     print_(model_name, get_default(model_name))
 
@@ -33,11 +36,10 @@ Model:          {name}
 {line}
 Iterations:     {iterations:.0e}
 Batch Size:     {batch_size}
-Optimizer:      {optimizer} 
 Learning Rate:  {learning_rate:.1e}
 {line}
 """.format(
-        line=line, name=model_name,  **parameters
+        line=line, name=model_name, **parameters
     )
     if dataset:
         message += """Dataset:        {dataset}
@@ -48,31 +50,14 @@ Learning Rate:  {learning_rate:.1e}
     print(message)
 
 
-def optimizer_identifier(parameters: dict) -> dict:
-    valid_config_fields = [
-        "learning_rate",
-        "beta_1",
-        "beta_2",
-        "epsilon",
-        "amsgrad",
-        "name",
-    ]
-
-    config = {key: parameters[key] for key in parameters if key in valid_config_fields}
-
-    return {"class_name": parameters["optimizer"], "config": config}
-
-
 betavae_mnist = {
     "iterations": 2e4,
     "batch_size": 128,
-    "optimizer": "adam",
     "learning_rate": 1e-4,
 }
 
 betavae_shapes3d = {
     "iterations": 5e5,
     "batch_size": 64,
-    "optimizer": "adam",
     "learning_rate": 1e-4,
 }
