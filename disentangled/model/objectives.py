cd29fd07eb0bb828a1295415510de965e02efbae
Elias Ågeby <eageby@kth.se>
2020-03-11 11:47:13 +0100

    restructured objectives

--- disentangled/model/objectives.py
+++ disentangled/model/objectives.py
@@ -11,64 +11,75 @@ class _Objective(tf.keras.layers.Layer):
     def call(self, input_):
         return self.objective(*[self.flatten(i) for i in input_])
 
+@tf.function
+def log_likelihood_gaussian(target, x_mean, x_log_var):
+    log_likelihood = -0.5 * tf.reduce_mean(
+        tf.reduce_sum(
+            x_log_var
+            + tf.square(target - x_mean) / tf.exp(x_log_var)
+            + tf.math.log(2 * math.pi),
+            axis=1,
+        ),
+        axis=0,
+    )
+
+    normalized_log_likelihood = log_likelihood + tf.reduce_mean(
+        -0.5 * tf.reduce_sum(x_log_var + tf.math.log(2 * math.pi), axis=1),
+        axis=0,
+    )
+
+    return log_likelihood, normalized_log_likelihood
+
+@tf.function
+def loglikelihood_bernoulli(target, x_mean, x_log_var):
+    return tf.reduce_mean(
+        tf.reduce_sum(
+            target * tf.math.log(x_mean) + (1 - target) * tf.math.log(1 - x_mean),
+            axis=1,
+        ),
+        axis=0,
+    )
+
+@tf.function
+def kld_gaussian(z_mean, z_log_var):
+    return tf.reduce_mean(
+        -0.5
+        * tf.reduce_sum(
+            1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1
+        ),
+        axis=0,
+    )
 
 class BetaVAE(_Objective):
     def __init__(self, beta=1, gaussian=False, **kwargs):
         super(BetaVAE, self).__init__(**kwargs)
         self.beta = beta
         self.gaussian = gaussian
+        import pdb;pdb.set_trace()
 
     @tf.function
-    def kld(self, z_mean, z_log_var):
-        return tf.reduce_mean(
-            -0.5
-            * tf.reduce_sum(
-                1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1
-            ),
-            axis=0,
-        )
-
-    @tf.function
-    def log_likelihood(self, target, x_mean, x_log_var):
-        if self.gaussian:
-            log_likelihood = -0.5 * tf.reduce_mean(
-                tf.reduce_sum(
-                    x_log_var
-                    + tf.square(target - x_mean) / tf.exp(x_log_var)
-                    + tf.math.log(2 * math.pi),
-                    axis=1,
-                ),
-                axis=0,
-            )
-
-            normalized_log_likelihood = log_likelihood + tf.reduce_mean(
-                -0.5 * tf.reduce_sum(x_log_var + tf.math.log(2 * math.pi), axis=1),
-                axis=0,
-            )
+    def objective(self, target, x_mean, x_log_var, z_mean, z_log_var):
+        if self.gaussian: 
+            log_likelihood, normalized_log_likelihood = log_likelihood_gaussian(target, x_mean, x_log_var)
 
             self.add_metric(
                 normalized_log_likelihood,
                 aggregation="mean",
                 name="Normalized loglikelihood",
             )
+        else:
+            log_likelihood = loglikelihood_bernoulli(target, x_mean, x_log_var)
 
-            return log_likelihood
+        kld = kld_gaussian(z_mean, z_log_var)
 
-        # bernoulli
-
-        return tf.reduce_mean(
-            tf.reduce_sum(
-                target * tf.math.log(x_mean) + (1 - target) * tf.math.log(1 - x_mean),
-                axis=1,
-            ),
-            axis=0,
-        )
-
-    @tf.function
-    def objective(self, target, x_mean, x_log_var, z_mean, z_log_var, beta=1):
-        log_likelihood = self.log_likelihood(target, x_mean, x_log_var)
-        kld = self.kld(z_mean, z_log_var)
         self.add_metric(-log_likelihood, aggregation="mean", name="-loglikelihood")
         self.add_metric(kld, aggregation="mean", name="kld")
 
-        return -log_likelihood + beta*kld
+        return -log_likelihood + self.beta*kld
+
+
+class FactorVAE(_Objective):
+    def __init__(self, gamma, **kwargs):
+        super().__init__(self, **kwargs) 
+        self.gamma = gamma
+
