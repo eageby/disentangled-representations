da876287b2bdeac7193100828ec5865b40e0b970
Elias Ågeby <eageby@kth.se>
2020-03-24 11:39:13 +0100

    Added custom parsing option class

--- disentangled/utils.py
+++ disentangled/utils.py
@@ -30,3 +30,44 @@ class TrainingProgress(tqdm.tqdm):
         self.postfix += ', '.join(key + ': ' + '{:.2f}'.format(metrics[key]) for key in metrics.keys())
 
         self.refresh()
+
+    def log(self, interval=1000):
+        if self.n % interval == 0:
+            message = "Iteration {} - ".format(self.n) + self.postfix
+            self.write(message) 
+
+"""https://stackoverflow.com/questions/54073767/command-line-interface-with-multiple-commands-using-click-add-unspecified-optio"""
+
+class AcceptAllCommand(click.Command):
+
+    def make_parser(self, ctx):
+        """Hook 'make_parser' and allow the opt dict to find any option"""
+        parser = super(AcceptAllCommand, self).make_parser(ctx)
+        command = self
+
+        class AcceptAllDict(dict):
+
+            def __contains__(self, item):
+                """If the parser does no know this option, add it"""
+
+                if not super(AcceptAllDict, self).__contains__(item):
+                    # create an option name
+                    name = item.lstrip('-')
+
+                    # add the option to our command
+                    click.option(item)(command)
+
+                    # get the option instance from the command
+                    option = command.params[-1]
+
+                    # add the option instance to the parser
+                    parser.add_option(
+                        [item], name.replace('-', '_'), obj=option)
+                return True
+
+        # set the parser options to our dict
+        parser._short_opt = AcceptAllDict(parser._short_opt)
+        parser._long_opt = AcceptAllDict(parser._long_opt)
+
+        return parser
+
