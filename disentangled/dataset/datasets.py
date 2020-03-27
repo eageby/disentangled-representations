173343bb9be8dfe27bb1b3b5edcd8bd9d15a49eb
Elias Ågeby <eageby@kth.se>
2020-03-27 17:02:37 +0100

    loop read

--- disentangled/dataset/datasets.py
+++ disentangled/dataset/datasets.py
@@ -7,7 +7,6 @@ import h5py
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import tensorflow_io as tfio
 
 from . import utils
 
@@ -79,16 +78,6 @@ class Shapes3d(Dataset):
 
     _builder = tfds.builder("{}:{}".format(_name, __version__))
 
-    factors = [
-        "label_floor_hue",
-        "label_wall_hue",
-        "label_object_hue",
-        "label_scale",
-        "label_shape",
-        "label_orientation",
-    ]
-    num_values_per_factor = [ 10, 10, 10, 8, 4, 15 ]
-
     @classmethod
     def pipeline(cls, split="train", batch_size=64, prefetch_batches=10):
         return (
@@ -110,78 +99,79 @@ class Shapes3d(Dataset):
 
         return {"image": element["image"] / 255, "label": labels}
 
-class Shapes3d_ordered(Shapes3d):
-    path = tf.keras.utils.get_file(
-        '3dshapes.h5', 'https://storage.googleapis.com/3d-shapes/3dshapes.h5')
+    class ordered:
+        factors = [
+            "label_floor_hue",
+            "label_wall_hue",
+            "label_object_hue",
+            "label_scale",
+            "label_shape",
+            "label_orientation",
+        ]
+        num_values_per_factor = [ 10, 10, 10, 8, 4, 15 ]
 
-    def load(self):
-        images = tfio.IODataset.from_hdf5(self.path, '/images')
-        labels = tfio.IODataset.from_hdf5(self.path, '/labels')
+        path = tf.keras.utils.get_file(
+            '3dshapes.h5', 'https://storage.googleapis.com/3d-shapes/3dshapes.h5')
 
-        def make_dict(image, label):
-            dataset = {}
-            dataset['image'] = image
-            dataset['label'] = label
+        @classmethod
+        def get_index(cls, factors):
+            """ Converts factors to indices in range(num_data)
+            Args:
+            factors: np array shape [6,batch_size].
+                     factors[i]=factors[i,:] takes integer values in
+                     range(_NUM_VALUES_PER_FACTOR[cls.factors[i]]).
 
-            return dataset
+            Returns:
+            indices: np array shape [batch_size].
+            """
+            indices = 0
+            base = 1
 
-        return tf.data.Dataset.zip((images, labels)).map(make_dict, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            for factor, _ in reversed(list(enumerate(cls.factors))):
+                indices += factors[factor] * base
+                base *= cls.num_values_per_factor[factor]
 
-    @classmethod
-    def get_index(cls, factors):
-        """ Converts factors to indices in range(num_data)
-        Args:
-        factors: np array shape [6,batch_size].
-                 factors[i]=factors[i,:] takes integer values in
-                 range(_NUM_VALUES_PER_FACTOR[cls.factors[i]]).
+            return indices
 
-        Returns:
-        indices: np array shape [batch_size].
-        """
-        indices = 0
-        base = 1
+        @classmethod
+        def batch_indices(cls, batch_size):
+            factors = [np.random.choice(cls.num_values_per_factor[f], batch_size) for f, _ in enumerate(cls.factors)]
 
-        for factor, _ in reversed(list(enumerate(cls.factors))):
-            indices += factors[factor] * base
-            base *= cls.num_values_per_factor[factor]
+            fixed_factor = np.random.choice(len(cls.factors))
+            fixed_factor_value = np.random.choice(cls.num_values_per_factor[fixed_factor])
+            factors[fixed_factor] = fixed_factor_value
+            return cls.get_index(factors), fixed_factor, fixed_factor_value
 
-        return indices
+        @classmethod
+        def generator(cls, batch_size):
+            for _ in itertools.count():
+                idx, factor, value = cls.batch_indices(batch_size)
+                idx = np.sort(idx) 
+                yield idx, factor, value 
 
-    @classmethod
-    def batch_indices(cls, batch_size):
-        factors = [np.random.choice(cls.num_values_per_factor[f], batch_size) for f, _ in enumerate(cls.factors)]
+        @classmethod
+        def read(cls):
+            data = h5py.File(cls.path, 'r', swmr=True)['images']
+            
+            def wrapper(idx, factor, value):
+                def inner(idx, factor, value):
+                    idx = np.asarray(idx)
+                    
+                    im = []
+                    for i in idx:
+                        im.append(data[i])
+                    im = np.stack(im) 
+                    return im / 255,  factor,  value
+                return tf.py_function(inner, inp=[idx, factor, value], Tout=(tf.float32, tf.int64, tf.int64)) 
 
-        fixed_factor = np.random.choice(len(cls.factors))
-        fixed_factor_value = np.random.choice(cls.num_values_per_factor[fixed_factor])
-        factors[fixed_factor] = fixed_factor_value
-        return cls.get_index(factors), fixed_factor, fixed_factor_value
+            return wrapper
 
-    @classmethod
-    def generator(cls, batch_size):
-        for _ in itertools.count():
-            idx, factor, value = cls.batch_indices(batch_size)
-            idx = np.sort(idx) 
-            yield idx, factor, value 
+        @classmethod
+        def create(cls, batch_size):
+            def dict_map(image, factor, value):
+                return {'image': image, 'factor':factor, 'factor_value': value}
 
-    @classmethod
-    def read(cls):
-        data = h5py.File(cls.path, 'r', swmr=True)['images']
-        
-        def wrapper(idx, factor, value):
-            def inner(idx, factor, value):
-                idx = np.asarray(idx)
-                print(idx)
-                return data[idx] / 255,  factor,  value
-            return tf.py_function(inner, inp=[idx, factor, value], Tout=(tf.float32, tf.int64, tf.int64)) 
+            index = tf.data.Dataset.from_generator(cls.generator, (tf.int64, tf.int64, tf.int64), output_shapes=((None), (), ()), args=(batch_size,))
+            dataset = index.map(cls.read())
 
-        return wrapper
-
-    @classmethod
-    def create(cls, batch_size):
-        def dict_map(image, factor, value):
-            return {'image': image, 'factor':factor, 'factor_value': value}
-
-        index = tf.data.Dataset.from_generator(cls.generator, (tf.int64, tf.int64, tf.int64), output_shapes=((None), (), ()), args=(batch_size,))
-        dataset = index.map(cls.read())
-
-        return dataset.map(dict_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            return dataset.map(dict_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
