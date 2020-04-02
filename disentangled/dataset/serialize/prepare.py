4331dfbc30b175064885d13bc14e37d3bbf18c4f
Elias Ågeby <eageby@kth.se>
2020-04-02 10:07:44 +0200

    moved data prep script

--- disentangled/dataset/serialize/prepare.py
+++ disentangled/dataset/serialize/prepare.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import click
 import disentangled.dataset.serialize.raw_datasets as raw_datasets
 from disentangled.dataset.serialize import write
+import disentangled.utils
 
 @click.command(
     context_settings=dict(
@@ -11,8 +12,13 @@ from disentangled.dataset.serialize import write
     )
 )
 @click.argument('dataset', type=click.Choice(raw_datasets.__all__, case_sensitive=False))
+@click.option('--gpu/--no-gpu', 'gpu', default=False)
 @click.option('--batches', default=1300, type=int)
 @click.option('--batch_size', default=100, type=int)
+@click.option('--overwrite', is_flag=True, default=False)
 @click.pass_context
-def cli(ctx, dataset, **kwargs):
+
+def cli(ctx,gpu, dataset, **kwargs):
+    if not gpu:
+        disentangled.utils.disable_gpu()
     write(raw_datasets.get(dataset), **kwargs)
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name='disentangled', version='0.1', packages=find_packages(),
        entry_points='''[console_scripts]
         disentangled=main:cli
-        disentangled-data=prepare:cli
+        disentangled-data=disentangled.dataset.serialize.prepare:cli
     ''',)
 
