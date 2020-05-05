055dbdf5e58dc2aa73df9423084f6b46c5c2388a
Elias Ågeby <eageby@kth.se>
2020-05-05 09:50:00 +0200

    Debugging metrics

--- disentangled/metric/factorvae.py
+++ disentangled/metric/factorvae.py
@@ -3,7 +3,6 @@ import disentangled.model.utils
 import disentangled.utils as utils
 import numpy as np
 import tensorflow as tf
-
 import scipy.stats
 
 def majority_voting_classifier(data, n_latent, n_generative):
@@ -12,59 +11,99 @@ def majority_voting_classifier(data, n_latent, n_generative):
     for d, k in data:
         V[d, k] += 1
 
+    import pdb;pdb.set_trace()
     return tf.math.argmax(V, axis=1)
+
+    # for k in range(n_factors):
+    #     v_matrix[j, k] = np.sum((d_values == j) & (k_values == k))
+
 
 def encode_dataset(model, data):
     def encoding(element):
-        representation = model.encode(element['image'])[0]
+        representation, representations_variance = model.encode(element['image'])
         element['representation'] = representation
+        element['representation_variance'] = representation_variance
         return element
 
     return data.map(encoding, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-def representation_variance(data):
+def representation_variance(data, subset=None):
     var = []
 
-    for batch in utils.TrainingProgress(data):
-        var.append(tf.math.reduce_variance(batch["representation"], axis=0))
+    if subset is not None:
+        data = data.take(subset)
+    progress = utils.TrainingProgress(data, total=subset)
+    progress.write('Calculating Empirical Variance')
+    for batch in progress:
+        var.append(batch["representation"])
 
-    return tf.reduce_mean(tf.stack(var), axis=0)
+    return tf.math.reduce_variance(tf.concat(var, 0), axis=0) 
 
-def intact_dimensions(dataset, significance_level=0.01):
+def intact_dimensions(data, subset=None, significance_level=0.05):
     representation_batches = []
 
-    for batch in utils.TrainingProgress(dataset):
+    if subset is not None:
+        data = data.take(subset)
+
+    progress = utils.TrainingProgress(data, total=subset)
+    progress.write('Calculating Collapsed Latent Dimensions')
+    for batch in progress:
         representation_batches.append(batch['representation'])
 
     representations = np.concatenate(representation_batches)
     pvalues = np.array([scipy.stats.kstest(representations[i], 'norm').pvalue for i in range(representations.shape[-1])])
-    return np.where(pvalues < significance_level)
+    idx =  np.where(pvalues < significance_level)[0]
+    print('{} collapsed dimensions'.format(representations.shape[-1] - idx.size))
+    return idx
 
-def metric_factorvae(model, dataset, training_votes=800, test_votes=500):
-    dataset = encode_dataset(model, dataset).take(training_votes+test_votes)
-    empirical_var = representation_variance(dataset)
-    intact_idx = intact_dimensions(dataset)
-    import pdb;pdb.set_trace()
+def intact_dimensions_kld(data,subset=None, tolerance=1e-2):
+    kld = []
+
+    if subset is not None:
+        data = data.take(subset)
+
+    progress = utils.TrainingProgress(data, total=subset)
+    progress.write('Calculating Collapsed Latent Dimensions')
+    for batch in progress:
+        mean = batch['representation'] 
+        log_var = batch['representation_variance'] 
+        kld.append(tf.reduce_mean(
+            0.5 * ( tf.exp(log_var) - log_var + tf.square(mean) - 1)
+            , axis=0))
+
+    kld = tf.reduce_mean(tf.stack(kld, axis=0), axis=0)
+    idx =  np.where(kld > tolerance)[0]
+    print('{} collapsed dimensions'.format(kld.shape[-1] - len(idx)))
+    return idx
+
+def metric(model, dataset, training_votes=800, test_votes=500, subset=None):
+    dataset = encode_dataset(model, dataset).take(training_votes)
+    empirical_var = representation_variance(dataset, subset)
+    # intact_idx = intact_dimensions(dataset, subset)
+    intact_idx = intact_dimensions_kld(dataset, subset)
 
     samples = [] 
-    for batch in dataset:
+    progress = disentangled.utils.TrainingProgress(dataset, total=training_votes)
+    progress.write('Calculating Metric Datapoints')
+    for batch in progress:
         representations = batch['representation'] 
 
         representations /= tf.math.sqrt(empirical_var)
         representations_variance = tf.math.reduce_variance(
             representations, axis=0)
-        dimension = tf.argmin(tf.gather(representations_variance, intact_idx), axis=1)
+        dimension = tf.gather(intact_idx, tf.argmin(tf.gather(representations_variance, intact_idx)))
 
-        samples.append(tf.stack((tf.squeeze(dimension), batch['factor'])))
+        samples.append(tf.stack((dimension, batch['factor'])))
 
     samples = tf.stack(samples)
 
-    train_data, test_data = tf.split(samples, [training_votes, test_votes], axis=0) 
+    classifier = majority_voting_classifier(samples, representations.shape[-1], 6)
 
-    classifier = majority_voting_classifier(train_data, representations.shape[-1], 6)
+    dimensions = samples[:, 0]
+    factors = samples[:, 1]
 
-    test_estimate = tf.gather(classifier, test_data[:, 0])
-    target = test_data[:, 1]
+    estimate = tf.gather(classifier, dimensions)
 
-    error_rate = tf.math.count_nonzero(target - test_estimate) / tf.size(target, out_type=tf.int64)
-    return error_rate
+    accuracy = np.sum(factors == estimate) / tf.size(dimensions, out_type=tf.int64)
+    import pdb;pdb.set_trace()
+    return accuracy
--- disentangled/metric/mig.py
+++ disentangled/metric/mig.py
@@ -3,7 +3,7 @@ import disentangled.model.utils
 import disentangled.utils as utils
 import numpy as np
 import tensorflow as tf
-from disentangled.model.objectives import log_likelihood_gaussian, logsumexp
+from disentangled.model.objectives import logsumexp, _TOLERANCE
 
 
 def entropy(mean, log_var):
@@ -11,11 +11,14 @@ def entropy(mean, log_var):
 
     return tf.reduce_mean(-log_likelihood, axis=0)
 
-
+def normalized_log_likelihood_gaussian(mean, log_var):
+    return -0.5*(tf.square(mean) / (tf.exp(log_var) + _TOLERANCE))
+ 
 def mutual_information_gap(model, dataset):
     factor_possibilities = tf.cast(
         np.array([10.0, 10.0, 10.0, 8.0, 4.0, 15.0]), dtype=tf.float32
     )
+
     log_prob_factors = -tf.math.log(factor_possibilities)
     factor_entropy = tf.math.log(factor_possibilities)
     log_px_condk = tf.reduce_sum(log_prob_factors) - log_prob_factors
@@ -24,12 +27,15 @@ def mutual_information_gap(model, dataset):
 
     for batch in dataset:
         z_mean, z_log_var = model.encode(batch["image"])
-        log_qzx = log_likelihood_gaussian(z_mean, z_log_var)
-        marginal_entropy = - \
-            tf.expand_dims(tf.reduce_mean(log_qzx, axis=0), axis=-1)
+        log_qzx = normalized_log_likelihood_gaussian(z_mean, z_log_var)
+
+        marginal_entropy = -tf.expand_dims(
+                    tf.reduce_mean(log_qzx, axis=0),
+                axis=-1)
+
         conditional_entropy = -logsumexp(
             tf.expand_dims(log_qzx, axis=-1) +
-            tf.reshape(log_px_condk, (1, 1, -1)),
+            tf.reshape(log_prob_factors, (1, 1, -1)),
             axis=0,
         )
 
@@ -37,17 +43,21 @@ def mutual_information_gap(model, dataset):
         mutual_information = tf.sort(
             mutual_information, axis=0, direction="DESCENDING")
 
+        normalized_mutual_information = mutual_information / factor_entropy
+
         mig.append(
             tf.reduce_mean(
-                (mutual_information[0, :] -
-                 mutual_information[1, :]) / factor_entropy
+                (normalized_mutual_information[0, :] -
+                 normalized_mutual_information[1, :]) 
             )
         )
 
+    import pdb;pdb.set_trace()
     return tf.reduce_mean(tf.stack(mig))
 
 
 if __name__ == "__main__":
-    dataset = disentangled.dataset.shapes3d.as_image_label().batch(1000).take(1)
+    disentangled.utils.disable_gpu()
+    dataset = disentangled.dataset.shapes3d.as_image_label().batch(128).take(10)
     model = disentangled.model.utils.load("betavae_shapes3d")
     print(mutual_information_gap(model, dataset))
