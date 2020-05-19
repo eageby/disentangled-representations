b94fb26721dddf89525c20fd68a4e9c7f560a22e
Elias Ågeby <eageby@kth.se>
2020-05-19 07:56:17 +0200

    Marginal entropy estimate correct

--- disentangled/metric/mig.py
+++ disentangled/metric/mig.py
@@ -1,63 +1,63 @@
-import disentangled.dataset as dataset
+import disentangled.dataset as dataset 
 import disentangled.model.utils
 import disentangled.utils as utils
+import disentangled.model.distributions as dist
 import numpy as np
 import tensorflow as tf
-from disentangled.model.objectives import logsumexp, _TOLERANCE
+from disentangled.model.objectives import _TOLERANCE
 
 
-def entropy(mean, log_var):
-    log_likelihood = log_likelihood_gaussian(mean, log_var)
-
-    return tf.reduce_mean(-log_likelihood, axis=0)
-
-def normalized_log_likelihood_gaussian(mean, log_var):
-    return -0.5*(tf.square(mean) / (tf.exp(log_var) + _TOLERANCE))
- 
 def mutual_information_gap(model, dataset):
     factor_possibilities = tf.cast(
         np.array([10.0, 10.0, 10.0, 8.0, 4.0, 15.0]), dtype=tf.float32
     )
 
-    log_prob_factors = -tf.math.log(factor_possibilities)
     factor_entropy = tf.math.log(factor_possibilities)
-    log_px_condk = tf.reduce_sum(log_prob_factors) - log_prob_factors
+    dataset_size = tf.reduce_prod(factor_possibilities)
+    
+    log_prob_factors = -tf.math.log(factor_possibilities)
+    log_px_condk = tf.reshape(tf.reduce_sum(log_prob_factors) - log_prob_factors, (1,1,-1))
+    log_prob_factors = tf.reshape(log_prob_factors, (1, -1))
 
-    mig = []
+    mutual_information = []
 
-    for batch in dataset:
-        z_mean, z_log_var = model.encode(batch["image"])
-        log_qzx = normalized_log_likelihood_gaussian(z_mean, z_log_var)
+    progress = disentangled.utils.TrainingProgress(dataset, total=480)
+    for batch in progress:
+        mean, log_var = model.encode(batch["image"])
+        noise = tf.random.normal(tf.shape(mean), mean=0.0, stddev=1.0)
+        samples = mean + tf.exp(0.5 * log_var) * noise
 
-        marginal_entropy = -tf.expand_dims(
-                    tf.reduce_mean(log_qzx, axis=0),
-                axis=-1)
+        batch_size = float(mean.shape[0])
 
-        conditional_entropy = -logsumexp(
-            tf.expand_dims(log_qzx, axis=-1) +
-            tf.reshape(log_prob_factors, (1, 1, -1)),
-            axis=0,
+        log_qzx_matrix = dist.Gaussian.log_likelihood(
+                dist.Gaussian, samples, tf.expand_dims(mean, axis=1), tf.expand_dims(log_var, axis=0)
         )
-
-        mutual_information = marginal_entropy - conditional_entropy
-        mutual_information = tf.sort(
-            mutual_information, axis=0, direction="DESCENDING")
-
-        normalized_mutual_information = mutual_information / factor_entropy
-
-        mig.append(
-            tf.reduce_mean(
-                (normalized_mutual_information[0, :] -
-                 normalized_mutual_information[1, :]) 
-            )
+        log_qz = tf.reduce_logsumexp(log_qzx_matrix, axis=1) - tf.math.log(batch_size * dataset_size)
+        
+        marginal_entropy = tf.reduce_mean(-log_qz, axis=0)
+        
+        log_qzx = dist.Gaussian.log_likelihood(
+                dist.Gaussian, tf.expand_dims(samples, -1), tf.expand_dims(mean,-1), tf.expand_dims(log_var,-1)
         )
+        conditional_entropy = - tf.reduce_mean(tf.expand_dims(log_qzx, -1) + log_px_condk, axis=0)
+        mutual_information.append(tf.expand_dims(marginal_entropy, -1) - conditional_entropy)
 
-    import pdb;pdb.set_trace()
+    mutual_information = tf.reduce_mean(tf.stack(mutual_information, 0), 0)
+    mutual_information = tf.sort(
+        mutual_information, axis=0, direction="DESCENDING")
+
+    normalized_mutual_information = mutual_information / factor_entropy
+    breakpoint()
+
+    mig = tf.reduce_mean(
+        normalized_mutual_information[0, :] -
+        normalized_mutual_information[1, :]
+    )
+
     return tf.reduce_mean(tf.stack(mig))
 
 
 if __name__ == "__main__":
-    disentangled.utils.disable_gpu()
-    dataset = disentangled.dataset.shapes3d.as_image_label().batch(128).take(10)
-    model = disentangled.model.utils.load("betavae_shapes3d")
+    dataset = disentangled.dataset.shapes3d.as_image_label().batch(1000)
+    model = disentangled.model.utils.load("factorvae_shapes3d")
     print(mutual_information_gap(model, dataset))
