69a1d8a4a7b4e04c499874241639895dd8c081c5
Elias Ågeby <eageby@kth.se>
2020-06-25 14:43:00 +0200

    Configurable betasvae

---
+++ disentangled/model/vae/beta_svae.py
@@ -0,0 +1,24 @@
+import gin.tf
+import tensorflow as tf
+
+import disentangled.model.distributions as dist
+import disentangled.model.networks as networks
+import disentangled.model.objectives as objectives
+import disentangled.utils
+
+from .vae import VAE
+
+@gin.configurable(module="model")
+class BetaSVAE(VAE):
+    def __init__(self, **kwargs):
+        super().__init__(name="BetaSVAE", **kwargs)
+
+    @tf.function
+    def sample(self, mean, log_var, training=False):
+        gammoid = tf.random.gamma([1], alpha=1, beta=1)
+
+        noise = tf.random.normal(shape=tf.shape(mean), mean=0.0, stddev=1.0)
+
+        return gammoid * mean + tf.math.sqrt(gammoid) * noise * tf.math.exp(
+            0.5 * log_var
+        )
