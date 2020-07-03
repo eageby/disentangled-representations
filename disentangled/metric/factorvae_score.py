c61cb754af1f32f359695251bc51a57444d1684b
Elias Ågeby <eageby@kth.se>
2020-07-03 12:14:33 +0200

    Factorvae training points

--- disentangled/metric/factorvae_score.py
+++ disentangled/metric/factorvae_score.py
@@ -21,19 +21,22 @@ def _majority_voting_classifier(data, n_latent, n_generative):
 
 @gin.configurable(module="disentangled.metric")
 def factorvae_score(
-    model, dataset, training_votes, test_votes, tolerance, progress_bar=True
+    model, dataset, training_points, test_points, tolerance, progress_bar=True
 ):
     empirical_var = disentangled.metric.utils.representation_variance(
         model, dataset)
     intact_idx = np.where(empirical_var > tolerance)[0]
 
-    fixed_factor_set = disentangled.metric.utils.fixed_factor_dataset(
-        dataset, batch_size=gin.REQUIRED, num_values_per_factor=gin.REQUIRED
-    ).take(training_votes + test_votes)
+    fixed_factor_set, batch_size = disentangled.metric.utils.fixed_factor_dataset(
+        dataset, batch_size=gin.REQUIRED, num_values_per_factor=gin.REQUIRED,
+    )
+    n_batches = int(tf.math.ceil((training_points + test_points) / batch_size))
+    fixed_factor_set = fixed_factor_set.take(n_batches)
+    n_factors = fixed_factor_set.element_spec['label'].shape[1]
 
     if progress_bar:
         fixed_factor_set = disentangled.utils.TrainingProgress(
-            fixed_factor_set, total=training_votes + test_votes
+            fixed_factor_set, total=n_batches
         )
         fixed_factor_set.write("{} intact dimensions".format(intact_idx.size))
         fixed_factor_set.write("Calculating Metric Datapoints")
@@ -55,9 +58,11 @@ def factorvae_score(
             tf.stack((dimension, tf.cast(batch["factor"], tf.int64))))
 
     samples = tf.stack(samples)
+    training_votes = int(training_points/batch_size)
+    test_votes = samples.shape[0] - training_votes
     train, test = tf.split(samples, [training_votes, test_votes])
 
-    classifier = _majority_voting_classifier(train, intact_idx.size, 6)
+    classifier = _majority_voting_classifier(train, intact_idx.size, n_factors)
 
     dimensions = test[:, 0]
     factors = test[:, 1]
