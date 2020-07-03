e8f3ad67ef75e6f366659f6c81b9570217e99baf
Elias Ågeby <eageby@kth.se>
2020-07-03 17:33:57 +0200

    one cli entry

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='disentangled', version='0.1', packages=find_packages(),
        entry_points='''[console_scripts]
-        disentangled-main=main:cli
-        disentangled-training=bins.disentangled_training:main
-        disentangled-prepare-data=bins.prepare:main
-        disentangled-template=bins.disentangled_template:main
+        disentangled=disentangled.cli.main:cli
     ''',)
 
